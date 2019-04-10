b5436e9d3d9f93fa3c0e5215ab95dfceb90a4dd6
nayeong <nayeong_e@naver.com>
2019-04-10 07:26:19 +0000

    Post 작성 기능 추가

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import PostModelForm
 
 # Create your views here.
 def create(request):
-    pass
+    if request.method == "POST" :
+        # 작성된 post를 DB에 적용
+        form = PostModelForm(request.POST)
+        if form.is_valid() :
+            form.save()
+            return redirect('post:create')
+    else :
+        # post를 작성하는 form을 보여 줌
+        form = PostModelForm()
+        return render(request, 'posts/create.html', {'form': form})
+
