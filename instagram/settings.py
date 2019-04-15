3a8726aee3888b34790c8229ae6179ba8eac7561
nayeong <nayeong_e@naver.com>
2019-04-15 01:09:24 +0000

    Django_extensions 추가

--- instagram/settings.py
+++ instagram/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['nayeong-instagram-naye0ng.c9users.io']
 # Application definition
 
 INSTALLED_APPS = [
+    'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -120,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL ='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
