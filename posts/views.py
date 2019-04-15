d34a374867916b462b3d803a89eefff78ed64048
nayeong <nayeong_e@naver.com>
2019-04-15 01:30:07 +0000

    Templates에 like기능 구현 및 like 액션에 @login-required 추가

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import PostModelForm
 from .models import Post
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def create(request):
@@ -40,9 +41,10 @@ def update(request, id) :
     form = PostModelForm(instance=post)
     return render(request,'posts/update.html',{'form': form})
 
+@login_required
 def like(request, post_id):
     #like를 추가할 post를 가져옴
-    post =get_object_or_404(Post,post_id)
+    post = get_object_or_404(Post,pk=post_id)
     # 만약 user가 해당 포스트를 이미 like했다면 like를 제거하고
     if request.user in post.like_users.all() :
         post.like_users.remove(request.user)
