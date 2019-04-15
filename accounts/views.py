9beae2421a611d69b3dad58aea94516c69617911
nayeong <nayeong_e@naver.com>
2019-04-15 04:41:54 +0000

    회원가입 구현

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 
@@ -20,3 +20,16 @@ def login(request) :
 def logout(request) :
     auth_logout(request)
     return redirect('posts:list')
+
+def signup(request) :
+    if request.method == "POST" :
+        form = UserCreationForm(request.POST)
+        if form.is_valid() :
+            user = form.save()
+            # 로그인처리까지하는데 이때 들어오는 password값은 2개가 들어오므로 user에서 받아온 결과물로 로그인 처리하자
+            # auth_login(request, request.POST)
+            auth_login(request, user)
+            return redirect('posts:list')
+    else :
+        form = UserCreationForm() 
+    return render(request, 'accounts/signup.html', {'form': form})
